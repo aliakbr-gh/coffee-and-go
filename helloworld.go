8cddad5854c4561181e6575606ac011e064fd7b1
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2026-03-26 13:54:18 +0500

    Comments in Go

--- helloworld.go
+++ helloworld.go
@@ -1,1 +1,15 @@
-package main; import ("fmt"); func main() { fmt.Println("Hello World!");}
+// This is a comment above main package
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	// This is a comment above print
+
+	/* The code below will print Hello World
+  	to the screen, and it is amazing */
+
+	fmt.Println("Hello World") // This is a also an example of comment
+}
